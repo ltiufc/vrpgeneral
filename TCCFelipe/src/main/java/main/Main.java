a9a38a97561decb0265cff3d4f4cf5d346b1f6e3
felipelima <f3lip303@gmail.com>
2020-03-12 11:26:37 -0300

    teste na main

--- TCCFelipe/src/main/java/main/Main.java
+++ TCCFelipe/src/main/java/main/Main.java
@@ -1,13 +1,20 @@
 package main;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import model.DC;
+import model.InstanciaGeral;
 import model.Params;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		DC dc = new DC(1,-39.222,-7.2999);
 		Params params = new Params();
@@ -21,7 +28,14 @@ public class Main {
 		String saida = gson.toJson(params);
 		
 		System.out.println(saida);
-
+		
+		String fileName = "/home/felipelima/eclipse-workspace/tccfelipe/exemploFormatoGeral.json";
+		String json = String.join(" ", Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8));
+		
+		InstanciaGeral instanciaGeral = new Gson().fromJson(json, InstanciaGeral.class);
+		
+		String saidaInstanciaGeral = gson.toJson(instanciaGeral);
+		System.out.println(saidaInstanciaGeral);
 	}
 
 }
