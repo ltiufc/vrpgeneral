4260f3ea29ce92750102c6c413ba0fd623407ae0
felipelima <f3lip303@gmail.com>
2020-03-12 11:25:34 -0300

    Criação de classe para instancia geral

---
+++ TCCFelipe/src/main/java/model/InstanciaGeral.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class InstanciaGeral {
+
+	private Params params;
+	private ArrayList<Places> places;
+	private ArrayList<Vehicles> vehicles;
+	private ArrayList<Orders> orders;
+}
