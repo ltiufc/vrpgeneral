ce2425c07869385e28412d49629a654a61df51d7
felipelima <f3lip303@gmail.com>
2020-04-08 10:09:10 -0300

    Ajustes para escrever arquivo json

--- TCCFelipe/src/main/java/main/Generator.java
+++ TCCFelipe/src/main/java/main/Generator.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import model.InstanciaGeral;
 
@@ -18,19 +19,20 @@ public class Generator {
 	public void recInstances() throws IOException {
 
 		Gson gson = new Gson();
-
+	
 		String fileName = "/home/felipelima/Documentos/Faculdade/TCC/testeSolomon.json";
 		String json = String.join(" ", Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8));
 		//Leitura do arquivo json e Transformação em Objeto.
 		instanciaGeral = new Gson().fromJson(json, InstanciaGeral.class);
 
 		
-		
 		String saidaInstanciaGeral = gson.toJson(instanciaGeral);
 		System.out.println(saidaInstanciaGeral);
 		
 		//Escrita do arquivo no formato json 
-		FileWriter arq = new FileWriter("/home/felipelima/eclipse-workspace/tccfelipe/TCCFelipe/src/main/java/data/Solomon.json");
+		FileWriter arq = new FileWriter("/home/felipelima/eclipse-workspace/tccfelipe/TCCFelipe/src/main/java/data/SolomonInstanceC101.json");
+		
 		arq.write(saidaInstanciaGeral);
+		arq.close();
 	}
 }
