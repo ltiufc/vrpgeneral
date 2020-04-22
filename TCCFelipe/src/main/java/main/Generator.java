274ee8702cd1c9e3fe2e1ca1d02f9071ebdcaf40
felipelima <f3lip303@gmail.com>
2020-04-22 16:33:40 -0300

    teste para deserialização de json

--- TCCFelipe/src/main/java/main/Generator.java
+++ TCCFelipe/src/main/java/main/Generator.java
@@ -10,6 +10,8 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.google.gson.Gson;
@@ -18,6 +20,9 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 
 import model.InstanciaGeral;
 
@@ -30,7 +35,23 @@ public class Generator {
 		// Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 		// Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
 
-		Gson gson = new Gson();
+		/*Gson gson = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new JsonDeserializer<LocalDateTime>() {
+			@Override
+			public LocalDateTime deserialize(JsonElement json, java.lang.reflect.Type typeOfT,
+					JsonDeserializationContext context) throws JsonParseException {
+				Instant instant = Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong());
+				return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+			}
+		}).create();*/
+		GsonBuilder gsonBuilder = new GsonBuilder();
+
+		gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
+
+		gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer());
+
+		Gson gson = gsonBuilder.setPrettyPrinting().create();
+		
+		// Gson gson = new Gson();
 		String fileName = "/home/felipelima/Documentos/Faculdade/TCC/Instâncias/testeSolomon.json";
 		String json = String.join(" ", Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8));
 		System.out.println(json);
@@ -49,5 +70,25 @@ public class Generator {
 
 		arq.write(saidaInstanciaGeral);
 		arq.close();
+	}
+
+	class LocalDateTimeDeserializer implements JsonDeserializer<LocalDateTime> {
+		@Override
+		public LocalDateTime deserialize(JsonElement json, java.lang.reflect.Type typeOfT,
+				JsonDeserializationContext context) throws JsonParseException {
+			return LocalDateTime.parse(json.getAsString(),
+					DateTimeFormatter.ofPattern("dd::MMM::uuuu HH::mm::ss").withLocale(Locale.ENGLISH));
+		}
+	}
+
+	class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
+		private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd::MMM::uuuu HH::mm::ss");
+
+		@Override
+		public JsonElement serialize(LocalDateTime src, java.lang.reflect.Type typeOfSrc,
+				JsonSerializationContext context) {
+			// TODO Auto-generated method stub
+			return new JsonPrimitive(formatter.format(src));
+		}
 	}
 }
