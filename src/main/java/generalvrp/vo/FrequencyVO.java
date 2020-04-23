b3eed4808f46b4ff8423e4b05b2a3bb0a6dc9327
felipelima <f3lip303@gmail.com>
2020-04-23 14:09:55 -0300

    Remoção de comentários para simplificar classe

--- src/main/java/generalvrp/vo/FrequencyVO.java
+++ src/main/java/generalvrp/vo/FrequencyVO.java
@@ -6,26 +6,5 @@ public class FrequencyVO {
 
 	public String type;
 	public ArrayList<String> daysOfWeekFixed;
-//	
-//	public FrequencyVO() {
-//		super();
-//	}
-//	public FrequencyVO(String type, ArrayList<String> daysOfWeekFixed) {
-//		super();
-//		setType(type);
-//		setDaysOfWeekFixed(daysOfWeekFixed);
-//	}
-//	public String getType() {
-//		return type;
-//	}
-//	public void setType(String type) {
-//		this.type = type;
-//	}
-//	public ArrayList<String> getDaysOfWeekFixed() {
-//		return daysOfWeekFixed;
-//	}
-//	public void setDaysOfWeekFixed(ArrayList<String> daysOfWeekFixed) {
-//		this.daysOfWeekFixed = daysOfWeekFixed;
-//	}
 	
 }
--- src/main/java/generalvrp/vo/LunchTimeVO.java
+++ src/main/java/generalvrp/vo/LunchTimeVO.java
@@ -9,40 +9,7 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 public class LunchTimeVO {
 
-	//private final DateTimeFormatter start = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-	
-	//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-DD'T'HH:MM:SS")
 	public Date start;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-DD'T'HH:MM:SS")
 	public Date end;
-	
-	//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "YYYY-MM-DD'T'HH:MM:SS")
-	//private LocalDateTime end;
 
-//	public LunchTimeVO() {
-//		super();
-//	}
-//
-//	public LunchTimeVO(LocalDateTime start, LocalDateTime end) {
-//		super();
-//		//setStart(start);
-//		//setEnd(end);
-//	}
-
-	/*public LocalDateTime getStart() {
-		return start;
-	}
-
-	public void setStart(LocalDateTime start) {
-		this.start = start;
-	}*/
-
-	/*public LocalDateTime getEnd() {
-		return end;
-	}
-
-	public void setEnd(LocalDateTime end) {
-		this.end = end;
-	}*/
 }
