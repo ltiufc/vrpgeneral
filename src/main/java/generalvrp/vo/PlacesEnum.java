e0b7392e7a4e0d497da0b58aee406883265c8b62
dmontieraragao <dmontier.aragao@ufc.br>
2020-05-05 13:35:31 -0300

    Ajuste no tipo de cada place para usar ENUM

---
+++ src/main/java/generalvrp/vo/PlacesEnum.java
@@ -0,0 +1,5 @@
+package generalvrp.vo;
+
+public enum PlacesEnum {
+	CD,CLIENT,GARAGE
+}
--- src/main/java/generalvrp/vo/PlacesVO.java
+++ src/main/java/generalvrp/vo/PlacesVO.java
@@ -1,19 +1,16 @@
 package generalvrp.vo;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.util.List;
 
 public class PlacesVO {
 
 	public int id;
 	public double latitude;
 	public double longitude;
-	public ArrayList<String> type = new ArrayList<String>();
-	public ArrayList<String> vehicleCompatible = new ArrayList<String>();
+	public List<PlacesEnum> type;
+	public ArrayList<String> vehicleCompatible;
 	public Date beginOperation;
 	public Date endOperation;
 
