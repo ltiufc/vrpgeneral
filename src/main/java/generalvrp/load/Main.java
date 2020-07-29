cc2adce27723e6ef28f9c88ba03db3351eadd6ba
felipelima <f3lip303@alu.ufc.br>
2020-07-28 23:41:10 -0300

    Push

--- src/main/java/generalvrp/load/Main.java
+++ src/main/java/generalvrp/load/Main.java
@@ -2,15 +2,18 @@ package generalvrp.load;
 
 import java.io.IOException;
 
+import entity.GeneralInstance;
+import generalvrp.vo.GeneralInstanceVO;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		LoadInstance loadInstance = new LoadInstance();
 		
-		loadInstance.recInstances("target/classes/data/solomon/SolomonInstanceC101.json");
-		
-		
+		GeneralInstance generalInstance  = loadInstance.recInstances("target/classes/data/solomon/SolomonInstanceC101.json");
+
+		System.out.println(generalInstance.toString());
 	}
 
 }
